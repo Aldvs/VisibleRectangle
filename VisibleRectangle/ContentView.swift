7a44c0ddbae136168f127a6c58b3d86fd8372cba
Andrew Gogol <andrewgogol777@gmail.com>
2024-05-13 15:42:21 +0300

    Update with lastOffset

--- VisibleRectangle/ContentView.swift
+++ VisibleRectangle/ContentView.swift
@@ -9,45 +9,51 @@ import SwiftUI
 
 struct ContentView: View {
     @State private var offset = CGSize.zero
-        
-        var body: some View {
-            ZStack {
-                VStack(spacing: 0) {
-                    Color.black
-                    Color.white
-                    Color.black
-                    Color.white
-                }
-                VStack(spacing: 0) {
-                    Color.white
-                    Color.pink
-                    Color.yellow
-                    Color.black
-                }
-                .overlay {
-                    dynamicRectangle
-                        .blendMode(.destinationOut)
-                }
-                .compositingGroup()
+    @State private var lastOffset = CGSize.zero
+    
+    var body: some View {
+        ZStack {
+            VStack(spacing: 0) {
+                Color.black
+                Color.white
+                Color.black
+                Color.white
             }
-            .ignoresSafeArea()
+            VStack(spacing: 0) {
+                Color.white
+                Color.pink
+                Color.yellow
+                Color.black
+            }
+            .overlay {
+                dynamicRectangle
+                    .blendMode(.destinationOut)
+            }
+            .compositingGroup()
         }
-        
-        var dynamicRectangle: some View {
-            RoundedRectangle(cornerRadius: 25.0)
-                .frame(width: 100, height: 100)
-                .foregroundColor(.white)
-                .offset(offset)
-                .gesture(
-                    DragGesture()
-                        .onChanged { gesture in
-                            self.offset = gesture.translation
-                        }
-                        .onEnded { _ in
-                        }
-                )
-        }
+        .ignoresSafeArea()
+    }
+    
+    var dynamicRectangle: some View {
+        RoundedRectangle(cornerRadius: 25.0)
+            .frame(width: 100, height: 100)
+            .foregroundColor(.white)
+            .offset(offset)
+            .gesture(
+                DragGesture()
+                    .onChanged { gesture in
+                        self.offset = CGSize(
+                            width: self.lastOffset.width + gesture.translation.width,
+                            height: self.lastOffset.height + gesture.translation.height
+                        )
+                    }
+                    .onEnded { gesture in
+                        self.lastOffset = self.offset // Сохраняем последнее положение
+                    }
+            )
+    }
 }
+
 
 #Preview {
     ContentView()
