a7b05ff2a15afd75520ebb46141f53cc928ec56f
Andrew Gogol <andrewgogol777@gmail.com>
2024-05-13 15:39:42 +0300

    Setup stasks and rect

--- VisibleRectangle/ContentView.swift
+++ VisibleRectangle/ContentView.swift
@@ -8,15 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+    @State private var offset = CGSize.zero
+        
+        var body: some View {
+            ZStack {
+                VStack(spacing: 0) {
+                    Color.black
+                    Color.white
+                    Color.black
+                    Color.white
+                }
+                VStack(spacing: 0) {
+                    Color.white
+                    Color.pink
+                    Color.yellow
+                    Color.black
+                }
+                .overlay {
+                    dynamicRectangle
+                        .blendMode(.destinationOut)
+                }
+                .compositingGroup()
+            }
+            .ignoresSafeArea()
         }
-        .padding()
-    }
+        
+        var dynamicRectangle: some View {
+            RoundedRectangle(cornerRadius: 25.0)
+                .frame(width: 100, height: 100)
+                .foregroundColor(.white)
+                .offset(offset)
+                .gesture(
+                    DragGesture()
+                        .onChanged { gesture in
+                            self.offset = gesture.translation
+                        }
+                        .onEnded { _ in
+                        }
+                )
+        }
 }
 
 #Preview {
