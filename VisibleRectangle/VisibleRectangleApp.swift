0971d1f6c36448978ce5487ce02ba6d7cb94aecb
Andrew Gogol <andrewgogol777@gmail.com>
2024-05-13 15:27:52 +0300

    Initial Commit

---
+++ VisibleRectangle/VisibleRectangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisibleRectangleApp.swift
+//  VisibleRectangle
+//
+//  Created by admin on 13.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct VisibleRectangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
